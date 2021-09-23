e15ccbbfc2bb760406793438a2364945096aabf9
EC2 Default User <423.r.takayama@gmail.com>
2021-09-23 07:54:18 +0000

    kan

--- bookers2/db/schema.rb
+++ bookers2/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_22_055910) do
+ActiveRecord::Schema.define(version: 2021_09_23_045031) do
 
   create_table "books", force: :cascade do |t|
     t.integer "user_id"
